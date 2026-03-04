4136fbd3021c44de53db464d1fbb0f075ecc6cb3
oleksandrperian-cpu <oleksandrperian@gmail.com>
2026-03-04 18:25:26 +0200

    Lab: Defined CryptoNode struct for network scanning

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+struct CryptoNode {
+    ip: String,
+    port: u16,
+}
+
+fn main() {
+    let my_scan = CryptoNode {
+        ip: String::from("127.0.0.1"),
+        port: 443,
+    };
+
+    println!("Scanning {} on port {}", my_scan.ip, my_scan.port);
+}
+s
